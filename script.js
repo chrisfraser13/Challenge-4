eefe8c980b8de5dc0ad8103738971a1097679b31
Chris Fraser <chrisfraser@Chriss-MBP.home>
2021-11-28 19:27:34 -0500

    continued to add to CSS and also added quiz questions to JS

--- script.js
+++ script.js
@@ -20,3 +20,55 @@ var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 
 // Object for Quiz questions
+var quizQuestions = [{
+    question: "What does HTML stand for?",
+    choiceA: "How to make labels",
+    choiceB: "Hypertext Markup Language",
+    choiceC: "Hefty Technological Material Language",
+    choiceD: "Hardware that makes Language",
+    correctAnswer: "b"},
+  {
+    question: "What does DOM stand for?",
+    choiceA: "Document Object Model",
+    choiceB: "Display Object Management",
+    choiceC: "Digital Ordinance Model",
+    choiceD: "Desktop Oriented Mode",
+    correctAnswer: "a"},
+   {
+    question: "What does CSS stand for?",
+    choiceA: "Cascading Style Sheets",
+    choiceB: "Collection of Styling Sheets",
+    choiceC: "Console Style Sheet",
+    choiceD: "Cute Style Sheet",
+    correctAnswer: "a"},
+    {
+    question: "When was Javascript invented?",
+    choiceA: "1987",
+    choiceB: "2004",
+    choiceC: "1995",
+    choiceD: "2013",
+    correctAnswer: "c"},
+    {
+    question: "When is localStorage data cleared?",
+    choiceA: "No expiration time",
+    choiceB: "On page reload",
+    choiceC: "On browser close",
+    choiceD: "On computer restart",
+    correctAnswer: "a"},  
+    {
+    question: "What does WWW stand for?",
+    choiceA: "Web World Withholds",
+    choiceB: "World Wide Web",
+    choiceC: "Wisdom, Withstand, Warrior",
+    choiceD: "Windy Weather Warning",
+    correctAnswer: "b"},
+    {
+    question: "What HTML attribute references an external JavaScript file?",
+    choiceA: "href",
+    choiceB: "src",
+    choiceC: "class",
+    choiceD: "index",
+    correctAnswer: "b"},
+        
+    
+    ];
